becfe6fed982bea467c3593da20c426bd6ce9095
Hicham <hicham_67@live.fr>
2024-12-12 22:46:35 +0100

    Ajout des nouveaux fichiers

---
+++ script.js
@@ -0,0 +1,13 @@
+/*const menuToggle = document.getElementById('menu-toggle');
+        const header = document.querySelector('header');
+
+        menuToggle.addEventListener('click', () => {
+            header.classList.toggle('menu-open');
+        });*/
+
+
+
+
+
+
+
